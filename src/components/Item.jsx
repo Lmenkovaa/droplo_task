95df41ac4d9a13231979668f6bb3180ae14de75b
Lizaveta Miankova <Lmenkovaaaa@gmail.com>
2024-11-27 22:38:30 +0100

    feat(menu-items): enhance to add end edit functionality for nested items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,30 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
+import MenuForm from "./MenuForm";
 
-const Item = ({ label, url, onAdd, onEdit, onDelete, level, children }) => {
+const Item = ({ id, label, url, onAdd, onEdit, onDelete, level, children }) => {
+  const [isAddFormVisible, setIsAddFormVisible] = useState(false);
+  const [isEditFormVisible, setIsEditFormVisible] = useState(false);
+
+  const handleFormSubmit = (newItem) => {
+    onAdd(id, newItem);
+    setIsAddFormVisible(false);
+  };
+
+  const handleEdit = (updatedItem) => {
+    onEdit(id, updatedItem);
+    setIsEditFormVisible(false);
+  };
+
   return (
     <div className="flex flex-shrink flex-col w-full">
-      <div className="flex items-center justify-between bg-white border-b border-secondary-200 py-4 px-5">
+      <div
+        className={`flex items-center justify-between bg-white border-b border-secondary-200 py-4 px-5 ${
+          level > 0 ? "rounded-bl-lg" : ""
+        } rounded-t-lg`}
+      >
         <div className="flex items-center gap-4">
           <Image src="/move.svg" alt="Move" width={24} height={24} />
           <div>
@@ -17,26 +36,45 @@ const Item = ({ label, url, onAdd, onEdit, onDelete, level, children }) => {
         <div className="inline-flex border border-gray-300 rounded-lg overflow-hidden min-h-10">
           <button
             className="text-sm font-semibold text-secondary-700 py-2.5 px-3.5 border-r border-gray-300"
-            onClick={onDelete}
+            onClick={() => onDelete(id)}
           >
             Usuń
           </button>
           <button
             className="text-sm font-semibold text-secondary-700 py-2.5 px-3.5"
-            onClick={onEdit}
+            onClick={() => setIsEditFormVisible(!isEditFormVisible)}
           >
             Edytuj
           </button>
           <button
             className="text-sm font-semibold text-secondary-700 py-2.5 px-3.5 border-l border-gray-300"
-            onClick={onAdd}
+            onClick={() => setIsAddFormVisible(!isAddFormVisible)}
           >
             Dodaj pozycję menu
           </button>
         </div>
       </div>
 
-      <div className={`flex flex-wrap ml-${level * 8}`}>{children}</div>
+      {isEditFormVisible && (
+        <div className="ml-9 mr-5 my-4">
+          <MenuForm
+            initialData={{ id, label, url }}
+            onSubmit={handleEdit}
+            onCancel={() => setIsEditFormVisible(false)}
+          />
+        </div>
+      )}
+
+      {isAddFormVisible && (
+        <div className="ml-9 mr-5 my-4">
+          <MenuForm
+            onSubmit={handleFormSubmit}
+            onCancel={() => setIsAddFormVisible(false)}
+          />
+        </div>
+      )}
+
+      <div className={`ml-${level * 8} flex flex-wrap ml-8`}>{children}</div>
     </div>
   );
 };
--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,38 +1,63 @@
 "use client";
 
+import { useState } from "react";
 import Item from "./Item";
+import MenuForm from "./MenuForm";
 
 const MenuList = ({ menuItems, onAdd, onEdit, onDelete }) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const handleAdd = (parentId, newItem) => {
+    onAdd(parentId, newItem);
+  };
+
+  const handleEdit = (id, updatedItem) => {
+    onEdit(id, updatedItem);
+  };
+
+  const handleAddNewItem = (newItem) => {
+    onAdd(null, newItem);
+    setIsFormOpen(false);
+  };
+
   const renderItems = (items, level = 0) =>
-    items.map((item) => (
+    items.map((item, index) => (
       <Item
         key={item.id}
         id={item.id}
         label={item.label}
         url={item.url}
-        onAdd={onAdd}
-        onEdit={onEdit}
+        onAdd={handleAdd}
+        onEdit={handleEdit}
         onDelete={onDelete}
         level={level}
+        className={index === 0 ? "first:rounded-t-lg" : ""}
       >
         {item.children?.length > 0 && renderItems(item.children, level + 1)}
       </Item>
     ));
 
+  if (menuItems.length === 0) {
+    return null;
+  }
+
   return (
     <div className="border bg-primary-100 rounded-lg w-full">
-      {menuItems.length > 0 ? (
-        renderItems(menuItems)
-      ) : (
-        <p className="text-center text-gray-500">Brak pozycji w menu</p>
-      )}
+      {renderItems(menuItems)}
       <div className="flex justify-start mx-6 my-5">
-        <button
-          onClick={onAdd}
-          className="px-3.5 py-2.5 bg-white text-secondary-700 text-sm font-semibold border border-gray-300 rounded-md"
-        >
-          Dodaj pozycję menu
-        </button>
+        {isFormOpen ? (
+          <MenuForm
+            onSubmit={handleAddNewItem}
+            onCancel={() => setIsFormOpen(false)}
+          />
+        ) : (
+          <button
+            onClick={() => setIsFormOpen(true)}
+            className="px-3.5 py-2.5 bg-white text-secondary-700 text-sm font-semibold border border-gray-300 rounded-md"
+          >
+            Dodaj pozycję menu
+          </button>
+        )}
       </div>
     </div>
   );
