aebabf72e7cb999fb6b5cf95667c9c5f6b27d1a7
Lizaveta Miankova <Lmenkovaaaa@gmail.com>
2024-11-27 22:11:23 +0100

    chore(config): update Tailwind config and package files

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,25 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          100: "#f5f5f5",
+          900: "#101828",
+        },
+        secondary: {
+          100: "#F9FAFB",
+          200: "#EAECF0",
+          400: "#475467",
+          700: "#344054",
+        },
+        white: "#FFFFFF",
+        gray:{
+          300: "#D0D5DD",
+          400: "#667085",
+        },
+        purple: {
+          100: "#D6BBFB",
+          300: "#6941C6",
+        }
       },
     },
   },
