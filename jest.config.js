e990a42087ce27b3c67fe80e3bdefb884076ed11
Lizaveta Miankova <Lmenkovaaaa@gmail.com>
2024-12-03 01:06:50 +0100

    chore(jest): format jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,20 @@
-   const nextJest = require('next/jest');
+const nextJest = require("next/jest");
 
-   const createJestConfig = nextJest({
-     dir: './',
-   });
+const createJestConfig = nextJest({
+  dir: "./",
+});
 
-   const customJestConfig = {
-     testEnvironment: 'jest-environment-jsdom',
-     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-     moduleNameMapper: {
-       '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-       '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-     },
-     transform: {
-       '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-     },
-     testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-   };
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+};
 
-   module.exports = createJestConfig(customJestConfig);
-   
+module.exports = createJestConfig(customJestConfig);
